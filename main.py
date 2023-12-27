6edae8844a5e92500bb4ee6913df53d3d402973c
NORXONDOR <100261200+NORXONDOR@users.noreply.github.com>
2023-12-27 21:49:41 +1100

    Proxy switching
    Added a system which allows for a list of proxies to be switched between as rate limits set in.   
    
    Proxies can be changed/added in the PROXIES list following the format given as example in main.py.

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import json
 import random
 
 
-DESIRED_LINKS_NUM = 50
+DESIRED_LINKS_NUM = 100
 
-BASE_PATH = "https://discord.com/billing/partner-promotions/1180231712274387115/"
-BASE_URL = 'https://api.discord.gx.games/v1/direct-fulfillment'
+# Sample proxies are given below. Only HTTP is necessary.
+# To get your own proxies, check https://free-proxy-list.net/ 
+PROXIES = (
+    None,     # Direct connection.
+    {'http': 'http://50.168.210.226:80'},
+    {'http': 'http://50.218.57.68:80'}
+)
 
 HEADERS = {
     'authority': 'api.discord.gx.games',
@@ -24,54 +29,89 @@ HEADERS = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0'
 }
 
-PROXIES = {
-    # Placeholder for avoiding rate limiting
-}
+BASE_PATH = "https://discord.com/billing/partner-promotions/1180231712274387115/"
+BASE_URL = 'https://api.discord.gx.games/v1/direct-fulfillment'
 
 
-def gen():
-    # Generates 64 character hex code to circumvent partnerUserId restriction
+def gen_link(proxy_idx, proxy_looped):
+    """
+    Generates and writes a Discord Nitro link to 'links.txt'.
+    
+    Args:
+        (int) proxy_idx: The index of the proxy from which the generator should start.   
+        (bool) proxy_looped: Whether or not all proxies have been used once.
+    
+    Returns:
+        (int) gen_state: state in which function finished: 0, 1 or 2.
+            0: no link added,
+            1: link added,
+            2: no link added and rate limited, 
+        (int) proxy_idx: The index of the proxy on which the generator finished.
+        (bool) proxy_looped: Whether or not all proxies have been used once.
+    """
+    gen_state = 0
+
+    # Generates 64 character hex code to circumvent partnerUserId restriction.
     hex_code = ''.join(random.choice('0123456789abcdef') for n in range(64))
 
     payload = {
         "partnerUserId": f"{hex_code}"
     }
-    
-    link_added = 0
-    rate_limited = 0
-    
-    response = requests.post(BASE_URL, json=payload, headers=HEADERS)
 
-    # Handles rate limiting
-    if response.status_code == 429:
-        print("429 Too Many Requests: Program has been rate limited.")
-        rate_limited = 1
-        return link_added, rate_limited
+    # Proxy rotation.
+    proxy_valid = False
+
+    while not proxy_valid:
+    
+        if proxy_idx == len(PROXIES):
+            proxy_idx = 0
+            proxy_looped = True
         
-    # Checks for successful response
+        response = requests.post(BASE_URL, json=payload, headers=HEADERS, proxies=PROXIES[proxy_idx])
+
+        # Handles rate limiting
+        if response.status_code == 429:
+            print("429 Too Many Requests: ", end='')
+            
+            if proxy_looped:
+                print("All proxies rate limited.")
+                gen_state = 2
+                return gen_state, proxy_idx, proxy_looped
+
+            print("Trying a different proxy.")
+            proxy_idx += 1
+            continue
+            
+        proxy_valid = True
+
+    # Checks for successful response.
     if response.status_code // 100 == 2:
         try:
             token = response.json().get('token', 'No token found')
             link = BASE_PATH + token
+            
             with open('links.txt', 'a') as file:
                 file.write(link + "\n")
+                
             print(f"Nitro link '...{link[108:138]}...' written.")
-            link_added = 1
+            gen_state = 1
         except json.JSONDecodeError:
             print("JSONDecodeError: Unable to parse response as JSON.")
             print("Response text:", response.text)
         except Exception as e:
             print("An unexpected error occurred:", str(e))
+            
     elif response.status_code == 504:
-        print("504 Gateway Timeout: Servers experiencing high traffic.")
+        print("504 Gateway Timeout: Servers experiencing high traffic. Trying again.")
+        gen_state, proxy_idx, proxy_looped = gen_link(proxy_idx, proxy_looped)
     else:
         print(f"Request failed with status code {response.status_code}")
         print("Response text:", response.text)
     
-    return link_added, rate_limited
+    return gen_state, proxy_idx, proxy_looped
 
 
-# UI start
+# UI start.
 with open('links.txt') as file:
     line_count = 0
     for line in file:
@@ -79,14 +119,19 @@ with open('links.txt') as file:
 
 print(f"'links.txt' starting with {line_count} links.")
 
+proxy_idx = 0
+proxy_looped = False
+
 added_lines = 0
-rate_limited = 0
 
-for i in range(DESIRED_LINKS_NUM):
-    print(f"{i+1}. ", end='')
-    link_added, rate_limited = gen()
-    if rate_limited:
+for link_num in range(DESIRED_LINKS_NUM):
+    print(f"{link_num+1}. ", end='')
+    gen_state, proxy_idx, proxy_looped = gen_link(proxy_idx, proxy_looped)
+    
+    if gen_state == 2:
+        print("Program stopping. Try adding more proxies to PROXIES to avoid rate limiting.")
         break
-    added_lines += link_added
+    else:
+        added_lines += gen_state
 
-print(f"'links.txt' finishing with {line_count + added_lines} links.")
+print(f"'links.txt' finishing with {line_count + added_lines} links ({added_lines} added).")
